5e0abf7f75bec56ff7de9541cea616a90bb0f59b
James Edward Gray II <james@graysoftinc.com>
2015-06-10 08:47:32 -0500

    Testing adding items.

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,9 +1,16 @@
 require_relative "../lib/to_do/list"
 
 describe ToDo::List do
+  let(:list) { ToDo::List.new }
+
   it "starts with empty categories" do
-    list = ToDo::List.new
     expect(list.not_done).to eq([ ])
     expect(list.done).to     eq([ ])
+  end
+
+  it "adds items to the not done category" do
+    item = double
+    list.add(double)
+    expect(list.not_done).to eq([double])
   end
 end
