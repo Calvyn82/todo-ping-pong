b612ef0949656fa4869f14230c140e60bddc5bfb
Calvyn82 <claytonflesher@gmail.com>
2015-06-10 09:08:29 -0500

    succesfully moves completed items to done

--- lib/to_do/list.rb
+++ lib/to_do/list.rb
@@ -1,21 +1,27 @@
 module ToDo
   class List
     def initialize
-      @items = []
+      @unfinished_items = []
+      @finished_items   = []
     end
 
-    attr_reader :items
+    attr_reader :unfinished_items, :finished_items
 
     def not_done
-      items
+      unfinished_items
     end
 
     def done
-      [ ]
+      finished_items
     end
 
     def add(item)
-      @items << item
+      @unfinished_items << item
+    end
+
+    def finish(item)
+      @unfinished_items.delete(item)
+      @finished_items << item
     end
   end
 end
