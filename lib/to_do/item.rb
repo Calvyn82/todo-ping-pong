d917d6a006b1bdc40d0399403c9f59cecea2c22c
Calvyn82 <claytonflesher@gmail.com>
2015-06-10 09:44:46 -0500

    Items now have a finished_at time

--- lib/to_do/item.rb
+++ lib/to_do/item.rb
@@ -2,8 +2,13 @@ module ToDo
   class Item
     def initialize(task)
       @task = task
+      @finished_at = nil
     end
 
-    attr_reader :task
+    attr_reader :task, :finished_at
+
+    def finish
+      @finished_at = Time.now
+    end
   end
 end
